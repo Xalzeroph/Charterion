f073aca76b6135c802f05cfa42b407b351d433e1
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-08 09:51:10 +0800

    feat: expose atomic native work batch mutation

--- src/nativeControl.ts
+++ src/nativeControl.ts
@@ -1,4 +1,5 @@
 export * from './nativeControlLegacy';
+export type { NativeWorkDocumentMutation } from './nativeControlPersistent';
 export {
   readNativeControlSnapshot,
   reportNativeBrowserRuntime,
@@ -6,6 +7,7 @@ export {
   readNativeWorkSnapshot,
   replaceNativeWorkState,
   mutateNativeWorkDocument,
+  batchMutateNativeWorkDocuments,
   reconcileNativeElasticFleet,
   reportNativeAgentRuntime,
   planNativeBrowserOperation,
