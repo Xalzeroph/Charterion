818b1109d8dcfe0268d135a740d5301c3bb6bacf
Xu Zihan <2024010904024@std.uestc.edu.cn>
2026-09-08 09:58:41 +0800

    arch: expose generated native protocol through control facade

--- src/nativeControl.ts
+++ src/nativeControl.ts
@@ -1,6 +1,15 @@
 export * from './nativeControlLegacy';
 export type { NativeWorkDocumentMutation } from './nativeControlPersistent';
 export {
+  NATIVE_CONTROL_HOST,
+  NATIVE_MAX_MESSAGE_BYTES,
+  NATIVE_RPC_METHODS,
+  NATIVE_RPC_DOMAINS,
+  NATIVE_RPC_METHOD_DOMAIN,
+  assertNativeRpcMethod,
+} from './nativeRpcProtocol.generated';
+export type { NativeRpcMethod, NativeRpcDomain } from './nativeRpcProtocol.generated';
+export {
   readNativeControlSnapshot,
   reportNativeBrowserRuntime,
   reportNativeAgentBrowser,
